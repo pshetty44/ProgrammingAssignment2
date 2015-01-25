76ca03da9d1cdc4f74f8d2a6bcf28963185e53b1
PJ <prabhanjan.shetty@gmail.com>
2015-01-25 03:36:24 -0800

    changes done

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The program computes the inverse of a sqare matrix.
+## If the inverse is already computed, it gets the inverse from the cache.
 
-## Write a short comment describing this function
+## This below function can cache the inverse of the given matrix.
+## This has been acheived through R's lexical scoping.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The below function uses the solve function to get the inverse of the 
+## given matrix.It checks if the inverse is already present in the cache
+## thereby avoiding costly computation.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data)
+  x$setinverse(i)
+  i
 }
